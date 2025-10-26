26ddbe214adea981cb7517d349a1e6afe54b7ecc
Ozeily <Ozeily@users.noreply.github.com>
2025-10-26 16:51:22 +0100

    comments

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 //DOM
-const form = document.querySelector('form');
+const form = document.querySelector('form'); //EDIT IF NECESSARY: select using IDs/classes if necessary
 const preview = document.querySelector('iframe');
 const copyBtn = document.querySelector('button');
 
-function cleanParams(params) { //remove unecessary params
+preview.addEventListener('load', () => { //set default values from widget.css :root
+    const iframeDoc = preview.contentDocument || preview.contentWindow.document;
+    const cssVars = getComputedStyle(iframeDoc.documentElement);
+
+    form.querySelectorAll('[name]').forEach(input => {
+        const varName = '--' + input.name;
+        const cssVarValue = cssVars.getPropertyValue(varName).trim();
+        //EDIT IF NECESSARY: add input types that don't depend on CSS variables in the array
+        if (!['checkbox', 'text', 'select-one'].includes(input.type)) { //select-one: select element (default value set in index.html)
+            input.value = cssVarValue;
+        }
+    });
+    sendSettings() //apply everything, including checkboxes & text (no diff for colours)
+});
+
+//DO NOT REMOVE OR EDIT ANYTHING BELOW, except lines 26 and 41, and lines 58 to 62
+function cleanParams(params) { //remove unecessary params. non vital function, but cleaner to keep
     for (const key in params) {
         const value = params[key]
 
-        if (value === null || value === undefined || typeof value === 'string' && value.trim() === "") {
+        if (value === null || value === undefined || typeof value === 'string' && value.trim() === "") { //remove last "|| ..." if you accept empty strings/spaces alone
             delete params[key] //delete all empty params
         }
     }
@@ -21,29 +37,15 @@ function sendSettings() { //send settings to widget
         params[cbox.name] = cbox.checked;
     });
 
+    //EDIT: change '*' to your domain name for security
     preview.contentWindow.postMessage({type: 'settings', payload: params}, '*') //send post msg to widget
 }
-
-preview.addEventListener('load', () => { //set default values from widget.css :root
-    const iframeDoc = preview.contentDocument || preview.contentWindow.document;
-    const cssVars = getComputedStyle(iframeDoc.documentElement);
-
-    form.querySelectorAll('[name]').forEach(input => {
-        const varName = '--' + input.name;
-        const cssVarValue = cssVars.getPropertyValue(varName).trim();
-        if (!['checkbox', 'text', 'select-one'].includes(input.type)) { //if in the array, there isn't input.type
-            input.value = cssVarValue;
-        }
-    });
-    sendSettings() //apply everything, including cboxes & text (no diff for colours)
-});
 
 //send settings whenever user enters input
 form.addEventListener('input', () => { sendSettings() })
 
 //url base for widget: website url + relative path to widget + ? to add params
 function getBaseURL() { return window.location.origin + '/widget/widget.html?'}
-
 
 //copy url function for button
 window.copyURL = function() {
@@ -53,8 +55,8 @@ window.copyURL = function() {
     const url = getBaseURL() + new URLSearchParams(params).toString(); //dynamic widget URL
 
     navigator.clipboard.writeText(url)
-    .then(() => { //display 'Copied!' on the button for 2s
-        copyBtn.innerText = 'Copied!'
+    .then(() => {  //EDIT TO YOUR LIKING
+        copyBtn.innerText = 'Copied!' //display 'Copied!' on the button for 2s
         setTimeout(() => { copyBtn.innerText = 'Copy URL' }, 2000)
     })
     .catch(err => { alert('Error: ' + err)})
--- widget/widget.js
+++ widget/widget.js
@@ -1,3 +1,5 @@
+//DO NOT REMOVE OR EDIT ANYTHING UNTIL THE 3 LINE COMMENT
+
 //DOM
 const textElt = document.querySelector('p');
 const widgetDiv = document.querySelector('#widget');
@@ -7,22 +9,6 @@ const root = document.documentElement; //to use css root variables
 
 function setVarValue(variable, value) { //set css root variable value
     root.style.setProperty(`--${variable}`, value)
-}
-
-function applySettings(params) {
-    for (const key in params) {
-        switch(key) {
-            case 'text': //change text if "text" param
-                textElt.innerText = params[key]
-                break //avoid doing the other cases
-            case 'rounded':
-                widgetDiv.classList.toggle('rounded', params[key])
-                break
-            default:
-                setVarValue(key, params[key]) //change css variable value (mainly for colours)
-        }
-    }
-    //call render function here if any
 }
 
 //form input reception
@@ -35,4 +21,24 @@ window.addEventListener('message', (event) => {
 //Embed handler
 if (params.toString()) { //if embedded
     applySettings(Object.fromEntries(params)) //directly apply settings from URL
+}
+
+//
+// BELOW IS WHAT YOU CAN EDIT (check comments)
+//
+
+function applySettings(params) {
+    for (const key in params) {
+        switch(key) { //EDIT TO YOUR LIKING: cases are for each setting that doesn't depend on a CSS variable
+            case 'text': //change text if "text" param
+                textElt.innerText = params[key]
+                break //avoid doing the other cases
+            case 'rounded':
+                widgetDiv.classList.toggle('rounded', params[key]) //toggle the 'rounded' class depending on the rounded param value
+                break
+            default:
+                setVarValue(key, params[key]) //change css variable value (mainly for colours)
+        }
+    }
+    //ADD: call render function here if necessary
 }
